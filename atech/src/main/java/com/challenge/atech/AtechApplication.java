4862f1e3a010fcba1ee6c1a456679bd5a9f14d68
Vinicius <vinidual@gmail.com>
2017-09-23 16:25:01 -0300

    Setting up the springboot application

---
+++ atech/src/main/java/com/challenge/atech/AtechApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.atech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtechApplication.class, args);
+	}
+}
