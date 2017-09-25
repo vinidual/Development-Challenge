3a44b4e40e67f5a143b7a79014e5c953665ef66e
Vinicius <vinidual@gmail.com>
2017-09-24 21:10:04 -0300

    Added some unit tests and alter atech folder name to atech-api

--- atech/src/test/java/com/challenge/atech/AtechApplicationTests.java
+++ atech/src/test/java/com/challenge/atech/AtechApplicationTests.java
@@ -1,17 +1,69 @@
 package com.challenge.atech;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.challenge.atech.entity.Airplane;
+import com.challenge.atech.entity.City;
+import com.challenge.atech.entity.Flight;
+import com.challenge.atech.entity.Pilot;
+import com.challenge.atech.repository.AirplaneRepository;
+import com.challenge.atech.repository.CityRepository;
+import com.challenge.atech.repository.FlightRepository;
+import com.challenge.atech.repository.PilotRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AtechApplicationTests {
 	
+	/**
+	 * Was accepted to inject the repositories directly and not mock them because we 
+	 * are working with the in-memory database H2
+	 */
+	
+	@Autowired
+	CityRepository cityRepository;
+	
+	@Autowired
+	PilotRepository pilotRepository;
+	
+	@Autowired
+	AirplaneRepository airplaneRepository;
+	
+	@Autowired
+	FlightRepository flightRepository;
+	
 	@Test
-	public void contextLoads() {
-		
+	public void shouldExistsTwoCitiesRecords() {
+		List<City> listCities = cityRepository.findAll();
+		assertEquals(listCities.size(), 2);
+	}
+	
+	@Test
+	public void shouldExistsOnePilotRecord() {
+		List<Pilot> listPilots = pilotRepository.findAll();
+		assertEquals(listPilots.size(), 1);
+	}
+	
+	@Test
+	public void shouldExistsOneAirplaneRecord() {
+		List<Airplane> listAirplanes = airplaneRepository.findAll();
+		assertEquals(listAirplanes.size(), 1);
+	}
+	
+	@Test
+	public void shouldExistsOneFlightRecord() {
+		List<Flight> listFlights = flightRepository.findAll();
+		assertEquals(listFlights.size(), 1);
 	}
 
+	
+	
 }
